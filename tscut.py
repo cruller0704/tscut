435bb1d2bf2c94cae6589dc9586b0133acf0485a
cruller <54399418+cruller0704@users.noreply.github.com>
2024-11-19 00:08:23 +0900

    Add back payload_unit_start_indicator check

--- tscut.py
+++ tscut.py
@@ -476,6 +476,7 @@ def frames(args):
                     if pts:
                         print(f'{pts:.6f},{picture_coding_type}')
 
+                if get_payload_unit_start_indicator(ts_packet) == 1:
                     video_pes = Pes(get_payload(ts_packet))
                     if video_pes.pts:
                         pts = video_pes.pts / 90000
@@ -555,6 +556,7 @@ def cut(args):
                             outpoint = packet_idx
                             break
 
+                if get_payload_unit_start_indicator(ts_packet) == 1:
                     video_pes = Pes(get_payload(ts_packet))
                     if video_pes.pts:
                         pts = video_pes.pts / 90000
